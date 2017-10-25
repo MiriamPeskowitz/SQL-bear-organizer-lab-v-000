5e6ca9919ea50e61e5a833d726ccff4a9efe459e
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-25 14:37:22 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,31 +1,31 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT bears.name, bears.age WHERE bears.gender = 'F';"
+  "SELECT bears.name, bears.age FROM BEARS WHERE bears.gender = 'F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT bears.name ORDER BY  bears.age ASC;"
+  "SELECT bears.name FROM bears ORDER BY  bears.age ASC;"
 
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT bears.name WHERE bears.alive = '1' and ORDER BY bears.age ASC;"
+  "SELECT bears.name FROM bears WHERE bears.alive = '1' and ORDER BY bears.age ASC;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECY bears.name, bears.age ORDER BY bears.age DESC LIMIT 1;"
+  "SELECY bears.name, bears.age FROM bears ORDER BY bears.age DESC LIMIT 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT bears.name, bears.age ORDER BY bears.age ASC LIMIT 1;"
+  "SELECT bears.name, bears.age FROM bears ORDER BY bears.age ASC LIMIT 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT bears.color GROUP BY bears.color SUM bears.color;"
+  "SELECT bears.color FROM bears GROUP BY bears.color SUM bears.color;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT bears.color GROUP BY temperament='goofy';"
+  "SELECT bears.color FROM bears GROUP BY temperament='goofy';"
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT bears.name WHERE temperament= 'aggressive';"
+  "SELECT bears.name FROM bears WHERE temperament= 'aggressive';"
 end
